407ddaf355575091fb8cd58251a1476336a72a5f
kimiagastya <kimiagastya@gmail.com>
2025-05-09 22:28:43 +0700

    information article errors fixed

--- modules/healthcare.information.article/healthcare/information/article/model/InformationImpl.java
+++ modules/healthcare.information.article/healthcare/information/article/model/InformationImpl.java
@@ -16,24 +16,60 @@ import healthcare.information.core.InformationComponent;
 @Table(name="information_article")
 public class InformationImpl extends InformationDecorator {
 
-	public String content;
-	public InformationImpl(
+    @Column(name="content", columnDefinition="TEXT")
+    public String content;
+    
+    public InformationImpl() {
         super();
         this.objectName = InformationImpl.class.getName();
     }
     
     public InformationImpl(String content) {
-    	super();
-		this.content = content;
-		this.objectName = InformationImpl.class.getName();
+        super();
+        this.content = content;
+        this.objectName = InformationImpl.class.getName();
     }
-	
-	public InformationImpl(InformationComponent record, String content) {
-		super(record);
-		this.content = content;
-		this.objectName = InformationImpl.class.getName();
-	}
+    
+    public InformationImpl(InformationComponent record, String content) {
+        super(record);
+        this.content = content;
+        this.objectName = InformationImpl.class.getName();
+    }
+    
+    @Override
+    public UUID getInformationId() {
+        return record.getInformationId();
+    }
 
+    @Override
+    public void setInformationId(UUID informationId) {
+        record.setInformationId(informationId);
+    }
 
+    @Override
+    public String getInformationTitle() {
+        return record.getInformationTitle();
+    }
 
+    @Override
+    public void setInformationTitle(String informationTitle) {
+        record.setInformationTitle(informationTitle);
+    }
+
+    @Override
+    public String getInformationDescription() {
+        return record.getInformationDescription();
+    }
+
+    @Override
+    public void setInformationDescription(String informationDescription) {
+        record.setInformationDescription(informationDescription);
+    }
+    
+    @Override
+    public HashMap<String, Object> toHashMap() {
+        HashMap<String, Object> map = record.toHashMap();
+        map.put("content", content);
+        return map;
+    }
 }
--- modules/healthcare.information.article/healthcare/information/article/resource/InformationResourceImpl.java
+++ modules/healthcare.information.article/healthcare/information/article/resource/InformationResourceImpl.java
@@ -3,98 +3,139 @@ import java.util.*;
 
 import vmj.routing.route.Route;
 import vmj.routing.route.VMJExchange;
+import vmj.hibernate.integrator.RepositoryUtil;
 
 import healthcare.information.core.InformationResourceDecorator;
 import healthcare.information.core.InformationImpl;
+import healthcare.information.core.InformationComponent;
+import healthcare.information.core.Information;
 import healthcare.information.core.InformationResourceComponent;
+import healthcare.information.InformationFactory;
 
 public class InformationResourceImpl extends InformationResourceDecorator {
-    public InformationResourceImpl (InformationResourceComponent record) {
+    private RepositoryUtil<Information> Repository;
+    
+    public InformationResourceImpl(InformationResourceComponent record) {
         super(record);
+        this.Repository = new RepositoryUtil<Information>(healthcare.information.core.InformationComponent.class);
     }
 
     // @Restriced(permission = "")
     @Route(url="call/article/save")
     public List<HashMap<String,Object>> save(VMJExchange vmjExchange){
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
-			return null;
-		}
-		  = create(vmjExchange);
-		Repository.saveObject();
-		return getAll(vmjExchange);
-	}
+        if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
+            return null;
+        }
+        Information information = create(vmjExchange);
+        Repository.saveObject(information);
+        return getAll(vmjExchange);
+    }
 
     public Information create(VMJExchange vmjExchange){
-		String content = (String) vmjExchange.getRequestBodyForm("content");
-		
-		  = record.create(vmjExchange);
-		 deco = Factory.create("healthcare.article.core.InformationImpl", , content);
-			return deco;
-	}
+        String content = (String) vmjExchange.getRequestBodyForm("content");
+        
+        // Get base information data via the parent implementation
+        HashMap<String, Object> baseInformationData = record.createInformation(vmjExchange);
+        
+        // Extract information from the base data
+        String informationTitle = (String) baseInformationData.get("informationTitle");
+        String informationDescription = (String) baseInformationData.get("informationDescription");
+        
+        // Create the base component
+        InformationComponent baseComponent = new healthcare.information.core.InformationImpl(
+            UUID.randomUUID(),
+            informationTitle,
+            informationDescription
+        );
+        
+        // Create the decorated information
+        Information deco = InformationFactory.createInformation(
+            "healthcare.information.article.InformationImpl", 
+            baseComponent, 
+            content
+        );
+        
+        return deco;
+    }
 
     public Information create(VMJExchange vmjExchange, int id){
-		String content = (String) vmjExchange.getRequestBodyForm("content");
-		  = Repository.getObject(id);
-		int recordId = (((Decorator) saved.getRecord()).getId();
-		
-		  = record.create(vmjExchange);
-		 deco = Factory.create("healthcare.article.core.InformationImpl", id, , content);
-			return deco;
-	}
+        String content = (String) vmjExchange.getRequestBodyForm("content");
+        Information saved = Repository.getObject(id);
+        InformationComponent baseRecord = (InformationComponent) saved;
+        
+        // Get updated information data
+        HashMap<String, Object> baseInformationData = record.createInformation(vmjExchange);
+        
+        // Extract information from the base data
+        String informationTitle = (String) baseInformationData.get("informationTitle");
+        String informationDescription = (String) baseInformationData.get("informationDescription");
+        
+        // Create the base component with existing ID
+        InformationComponent baseComponent = new healthcare.information.core.InformationImpl(
+            baseRecord.getInformationId(),
+            informationTitle,
+            informationDescription
+        );
+        
+        // Create the decorated information
+        Information deco = InformationFactory.createInformation(
+            "healthcare.information.article.InformationImpl", 
+            baseComponent, 
+            content
+        );
+        
+        return deco;
+    }
 
     // @Restriced(permission = "")
     @Route(url="call/article/update")
     public HashMap<String, Object> update(VMJExchange vmjExchange){
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
-			return null;
-		}
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		
-		  = Repository.getObject(id);
-		 = create(vmjExchange, id);
-		
-		Repository.updateObject();
-		 = Repository.getObject(id);
-		//to do: fix association attributes
-		
-		return .toHashMap();
-		
-	}
+        if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
+            return null;
+        }
+        String idStr = (String) vmjExchange.getRequestBodyForm("id");
+        int id = Integer.parseInt(idStr);
+        
+        Information information = Repository.getObject(id);
+        information = create(vmjExchange, id);
+        
+        Repository.updateObject(information);
+        information = Repository.getObject(id);
+        
+        return information.toHashMap();
+    }
 
-	// @Restriced(permission = "")
+    // @Restriced(permission = "")
     @Route(url="call/article/detail")
     public HashMap<String, Object> get(VMJExchange vmjExchange){
-		return record.getInformation(vmjExchange);
-	}
+        return record.getInformation(vmjExchange);
+    }
 
-	// @Restriced(permission = "")
+    // @Restriced(permission = "")
     @Route(url="call/article/list")
     public List<HashMap<String,Object>> getAll(VMJExchange vmjExchange){
-		List<> List = Repository.getAllObject("_impl");
-		return transformListToHashMap(List);
-	}
+        List<Information> list = Repository.getAllObject("information_impl");
+        return transformListToHashMap(list);
+    }
 
-    public List<HashMap<String,Object>> transformListToHashMap(List<> List){
-		List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
-        for(int i = 0; i < List.size(); i++) {
-            resultList.add(List.get(i).toHashMap());
+    public List<HashMap<String,Object>> transformListToHashMap(List<Information> list){
+        List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
+        for(int i = 0; i < list.size(); i++) {
+            resultList.add(list.get(i).toHashMap());
         }
-
         return resultList;
-	}
+    }
 
-	// @Restriced(permission = "")
+    // @Restriced(permission = "")
     @Route(url="call/article/delete")
     public List<HashMap<String,Object>> deleteInformation(VMJExchange vmjExchange){
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
-			return null;
-		}
-		
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		Repository.deleteObject(id);
-		return getAll(vmjExchange);
-	}
-
+        if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
+            return null;
+        }
+        
+        String idStr = (String) vmjExchange.getRequestBodyForm("id");
+        int id = Integer.parseInt(idStr);
+        Repository.deleteObject(id);
+        return getAll(vmjExchange);
+    }
 }
