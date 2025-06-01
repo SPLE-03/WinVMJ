79428551054b620d2f1f59245d094fd43d146c26
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-06-01 15:10:37 +0700

    feat: temansehat backend

--- src-gen/Hell/run_all.sh
+++ src-gen/Hell/run_all.sh
@@ -10,9 +10,9 @@ trap cleanup SIGINT
 
 read -p "Enter the path to the frontend directory: " frontend_dir
 
-echo "SELECT 'CREATE DATABASE healthcare_product_hell' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthcare_product_hell') \gexec" | psql "postgresql://postgres:MacKebun134@localhost"
+echo "SELECT 'CREATE DATABASE healthcare_product_hell' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthcare_product_hell') \gexec" | psql "postgresql://postgres:postgres@localhost"
 for file in sql/*.sql; do
-    psql -a -f "$file" "postgresql://postgres:MacKebun134@localhost/healthcare_product_hell"
+    psql -a -f "$file" "postgresql://postgres:postgres@localhost/healthcare_product_hell"
 done
 
 java -cp healthcare.product.hell --module-path healthcare.product.hell -m healthcare.product.hell &
